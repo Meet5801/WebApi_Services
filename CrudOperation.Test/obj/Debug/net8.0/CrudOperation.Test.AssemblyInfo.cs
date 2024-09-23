7421ea4f07631a2923dc41dfaae030ae0db37da0
Meet5801 <meetsuthar217@gmail.com>
2024-09-23 12:38:45 +0530

    doneeeee

--- CrudOperation.Test/obj/Debug/net8.0/CrudOperation.Test.AssemblyInfo.cs
+++ CrudOperation.Test/obj/Debug/net8.0/CrudOperation.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrudOperation.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+996b14cee7969a2c6250f5d09addae77f8260928")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cc494405c955deeab9abbf95a289fe0fa80259b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrudOperation.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrudOperation.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
