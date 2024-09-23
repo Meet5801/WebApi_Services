5e0a7e81a204594d3f4119adfcaf05906b93f4f1
Meet5801 <meetsuthar217@gmail.com>
2024-09-23 12:13:52 +0530

    done

---
+++ Crud_Operation/Services/OtpService/otpService.cs
@@ -0,0 +1,67 @@
+﻿using OfficeOpenXml.Packaging.Ionic.Zip;
+using Twilio;
+using Twilio.Exceptions;
+using Twilio.Rest.Verify.V2.Service;
+
+namespace Crud_Operation.Services.OtpService
+{
+    public class otpService : IotpService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string accountSid;
+        private readonly string authToken;
+        private readonly string verificationCode;
+
+        public otpService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            accountSid = _configuration["Twilio:AccountSid"];
+            authToken = _configuration["Twilio:AuthToken"];
+            verificationCode = _configuration["Twilio:VerificationCode"];
+        }
+             
+        public async Task<VerificationResource> SendOTP(string phoneNumber)
+        {
+            TwilioClient.Init(accountSid, authToken);
+
+            try
+            {
+                var verificationResource = await VerificationResource.CreateAsync(
+                    to: phoneNumber,
+                    channel: "sms",
+                    pathServiceSid: verificationCode
+                );
+
+                return verificationResource;
+            }
+            catch (ApiException ex)
+            {
+                // Log the detailed error
+                Console.WriteLine($"Twilio API error: {ex.Message} - {ex.Code}");
+                throw;
+            }
+        }
+
+        public async Task<VerificationCheckResource> VerifyOTP(string phoneNumber, string otp)
+        {
+            TwilioClient.Init(accountSid, authToken);
+
+            try
+            {
+                var verificationCheckResource = await VerificationCheckResource.CreateAsync(
+                    to: phoneNumber,
+                    code: otp,
+                    pathServiceSid: verificationCode
+                );
+
+                return verificationCheckResource;
+            }
+            catch (ApiException ex)
+            {
+                // Log the detailed error
+                Console.WriteLine($"Twilio API error: {ex.Message} - {ex.Code}");
+                throw;
+            }
+        }
+    }
+}
